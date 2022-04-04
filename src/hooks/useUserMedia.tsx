58dfc7556dd706c140be72aa4b7042fb602e3f4e
degurii <sansam709@gmail.com>
2022-04-05 00:12:02 +0900

    feat: [#4] useUserMedia 훅 구현
    - media stream을 반환하는 훅임
    - video, audio constraints를 분해하여 각각의 stream에서
      track 추출 후 합친 뒤 하나의 stream으로 반환 함
    - constraints가 잘못되었거나, 에러가 발생한 경우 각 스트림마다
      비어있는 트랙을 반환하도록 함

---
+++ src/hooks/useUserMedia.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
+const getMediaTracksWithConstraints = async (
+  constraints: MediaStreamConstraints,
+) => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    return [...stream.getTracks()];
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
+
+export function useUserMedia(constraints: MediaStreamConstraints) {
+  const [userMediaStream, setUserMediaStream] = useState<MediaStream>();
+
+  useEffect(
+    function () {
+      const getUserMedia = async () => {
+        const videoTracks = await getMediaTracksWithConstraints({
+          video: constraints.video,
+        });
+        const audioTracks = await getMediaTracksWithConstraints({
+          audio: constraints.audio,
+        });
+        const tracks = [...videoTracks, ...audioTracks];
+        const stream = new MediaStream(tracks);
+
+        setUserMediaStream(stream);
+      };
+
+      getUserMedia();
+    },
+    [constraints],
+  );
+
+  return userMediaStream;
+}
