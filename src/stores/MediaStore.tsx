5f9dbadcbe1f01fe380c3a27226e45dda8524959
degurii <sansam709@gmail.com>
2022-04-07 23:11:48 +0900

    feat: [#4] 마이크, 카메라 장치 선택 기능
    - 현재 선택된 장치인 selectedDevice를 유지
    
    - selectedDevice가 변경될 때 마다 새로운 stream을 생성함
    
    - 기기에 연결된 미디어 디바이스 목록이 변경된 경우(ex. 에어팟 연결 등)
      selectedDevice를 브라우저에 설정된 default device로 변경하도록 함

--- src/stores/MediaStore.tsx
+++ src/stores/MediaStore.tsx
@@ -1,32 +1,91 @@
-import { createContext, PropsWithChildren } from 'react';
-import { useEnumerateDevice } from '../hooks/useEnumerateDevice';
+import {
+  createContext,
+  PropsWithChildren,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import {
+  useEnumerateDevice,
+  Devices,
+  initialDevices,
+} from '../hooks/useEnumerateDevice';
 import { useUserMediaStream } from '../hooks/useUserMediaStream';
 
 type MediaContextType = {
-  userMediaStream: MediaStream | null;
-  devices: MediaDeviceInfo[];
+  userMediaStream: MediaStream;
+  devices: Devices;
+  selectedDevice: SelectedDevice;
+  changeSelectedDevice: (kind: MediaDeviceKind, deviceId: string) => void;
 };
 
 type Props = {};
 
-export const MediaContext = createContext<MediaContextType>({
-  userMediaStream: null,
-  devices: [],
-});
+export type SelectedDevice = Record<MediaDeviceKind, MediaDeviceInfo | null>;
 
-const mediaStreamConstraints: MediaStreamConstraints = {
-  video: true,
+const initialSelectedDevice: SelectedDevice = {
+  audioinput: null,
+  audiooutput: null,
+  videoinput: null,
 };
 
+export const MediaContext = createContext<MediaContextType>({
+  userMediaStream: new MediaStream(),
+  devices: initialDevices,
+  selectedDevice: initialSelectedDevice,
+  changeSelectedDevice: () => {},
+});
+
 export const MediaStore = (props: PropsWithChildren<Props>) => {
-  const userMediaStream = useUserMediaStream(mediaStreamConstraints);
+  const [selectedDevice, setSelectedDevice] = useState<SelectedDevice>(
+    initialSelectedDevice,
+  );
+
+  // TODO:
+  // 1. constraints 세분화(size 등)
+  const constraints: MediaStreamConstraints = useMemo(() => {
+    const videoinputDeviceId = selectedDevice.videoinput?.deviceId;
+    const audioinputDeviceId = selectedDevice.audioinput?.deviceId;
+    return {
+      video: {
+        deviceId: videoinputDeviceId && {
+          exact: videoinputDeviceId,
+        },
+      },
+      audio: {
+        deviceId: audioinputDeviceId && {
+          exact: audioinputDeviceId,
+        },
+      },
+    };
+  }, [selectedDevice]);
+
+  const userMediaStream = useUserMediaStream(constraints);
   const devices = useEnumerateDevice();
+
+  const changeSelectedDevice = (kind: MediaDeviceKind, deviceId: string) => {
+    setSelectedDevice({
+      ...selectedDevice,
+      [kind]:
+        devices[kind].find((device) => device.deviceId === deviceId) ?? null,
+    });
+  };
+
+  useEffect(() => {
+    setSelectedDevice({
+      videoinput: devices.videoinput[0] ?? null,
+      audioinput: devices.audioinput[0] ?? null,
+      audiooutput: devices.audiooutput[0] ?? null,
+    });
+  }, [devices]);
 
   return (
     <MediaContext.Provider
       value={{
         userMediaStream,
         devices,
+        selectedDevice,
+        changeSelectedDevice,
       }}
     >
       {props.children}
